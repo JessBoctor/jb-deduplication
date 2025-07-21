24b6faa32399eda608dd8ab8d4a1334ab9dd2cea
Jess Boctor <contactme@jessboctor.com>
2025-07-21 14:24:10 -0500

    Initialize plugin

---
+++ jb-deduplication.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: JB Deduplication
+ * Description: Removes duplicate posts and media files from WordPress.
+ * Version: 1.0.0
+ * Author: Jess Boctor
+ * License: GPL2
+ */
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+define( 'SCUD_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
